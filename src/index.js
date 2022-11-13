f89ecd1a00efadaeb767ae84b51752981893e706
Ming Zhang <mzhang630@gmail.com>
2022-11-13 03:06:16 -0500

    finished dynamically creating players list from data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-import * as allPlayersInfo from "./scripts/fetchPlayers";
+// import * as allPlayersInfo from "./scripts/fetchPlayers";
 
-allPlayersInfo.getPlayersData();
+// allPlayersInfo.getPlayersData();
+
+//fetching from local host may cause problems will not use for now
+
+import teams from "./data/teams.json";
+
+// teams.forEach((team) => console.log(team));
+
+//iterate through array
+// teams.forEach((team) => {
+//   team.players.forEach();
+// });
+
+// creating a list of players dynamically from array
+let playerSelectionContainer = document.getElementById(
+  "player-selection-container"
+);
+
+function createPlayersList() {
+  teams.forEach((team) => {
+    team.players.forEach((player) => {
+      let playerContainer = document.createElement("div");
+      playerContainer.classList.add("player-container");
+      playerSelectionContainer.appendChild(playerContainer);
+
+      let players = document.createElement("div");
+      players.classList.add("players");
+      players.setAttribute("id", player.nickname.replace(/ /g, ""));
+      // console.log(player.nickname);
+      playerContainer.appendChild(players);
+
+      let teamLogo = document.createElement("img");
+      teamLogo.title = player.nickname;
+      teamLogo.src = `${team.logo}`;
+      teamLogo.alt = player.nickname;
+      players.appendChild(teamLogo);
+
+      let playerName = document.createElement("div");
+      playerName.classList.add("player-name");
+      players.appendChild(playerName);
+      playerName.textContent += player.nickname;
+    });
+  });
+}
+
+createPlayersList();
--- src/scripts/fetchPlayers.js
+++ src/scripts/fetchPlayers.js
@@ -1,15 +1,15 @@
-export const getPlayersData = async function () {
-  fetch("http://localhost:5500/dist/teams.json")
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return res.json();
-    })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-};
+// export const getPlayersData = async function () {
+//   fetch("../teams.json")
+//     .then((res) => {
+//       if (!res.ok) {
+//         throw new Error("Network response was not ok");
+//       }
+//       return res.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// };
