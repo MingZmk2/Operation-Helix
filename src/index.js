e57eb60c7848119a2f051be1a6af6ecf5a84925c
Ming Zhang <mzhang630@gmail.com>
2022-11-15 15:41:04 -0500

    added  modal function

--- src/index.js
+++ src/index.js
@@ -391,3 +391,16 @@ function renderPlayerRating(player, playerDataInfo) {
 }
 
 createCardForPlayer();
+
+//modal script
+const open = document.getElementById("open");
+const modal_container = document.getElementById("modal_container");
+const close = document.getElementById("close");
+
+open.addEventListener("click", () => {
+  modal_container.classList.add("show");
+});
+
+close.addEventListener("click", () => {
+  modal_container.classList.remove("show");
+});
